0e557fab1839182eaed5d84c5e504c6cab428bd2
giorgigobechia <giorgi.gobechia77@gmail.com>
2022-01-20 15:56:25 +0400

    added smooth scrolling feature

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+//MODAL
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1')
 
 const openModal = function (e) {
     e.preventDefault()
@@ -28,3 +28,16 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+//SMOOTH SCROLL
+btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+        section1.scrollIntoView({behavior: 'smooth'})
+})
+document.querySelector('.nav__links').addEventListener('click',function (e) {
+    e.preventDefault()
+
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href')
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+})
