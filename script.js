769c5ce871aec49752d10611a1876373727a48b6
giorgigobechia <giorgi.gobechia77@gmail.com>
2022-01-20 19:00:39 +0400

    added carousel feature

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+//SMOOTH SCROLL
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1')
+//TABBED COMPONENT
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
 
 const openModal = function (e) {
     e.preventDefault()
@@ -40,4 +46,12 @@ document.querySelector('.nav__links').addEventListener('click',function (e) {
         const id = e.target.getAttribute('href')
         document.querySelector(id).scrollIntoView({behavior: 'smooth'})
     }
+})
+tabsContainer.addEventListener('click',function (e) {
+    const clicked = e.target.closest('.operations__tab')
+    if (!clicked) return
+    tabs.forEach(t => t.classList.remove('operations__tab--active'))
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+    clicked.classList.add('operations__tab--active')
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 })
