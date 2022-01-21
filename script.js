1d95cbb66a276e02e76d95f76b348aeaf267c88a
giorgigobechia <giorgi.gobechia77@gmail.com>
2022-01-21 16:52:28 +0400

    added sticky navigation

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ const section1 = document.querySelector('#section--1')
 const tabs = document.querySelectorAll('.operations__tab')
 const tabsContainer = document.querySelector('.operations__tab-container')
 const tabsContent = document.querySelectorAll('.operations__content')
-
+//NAVIGATION
+const nav = document.querySelector('.nav')
+const header = document.querySelector('.header')
 
 const openModal = function (e) {
     e.preventDefault()
@@ -58,3 +60,29 @@ tabsContainer.addEventListener('click',function (e) {
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
 })
 // MENU FADE ANIMATION WITH JS
+const handleHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+        const logo = link.closest('.nav').querySelector('img')
+
+        siblings.forEach(el => {
+            if (el !== link) el.style.opacity = this
+        })
+        logo.style.opacity = this
+    }
+}
+nav.addEventListener('mouseover',handleHover.bind(0.5))
+nav.addEventListener('mouseout',handleHover.bind(1))
+//sticky navigation
+const stickyNav = function (entries) {
+    const [entry] = entries
+    console.log(entry);
+    !entry.isIntersecting ? nav.classList.add('sticky') :  nav.classList.remove('sticky')
+}
+const headerObserver = new IntersectionObserver(stickyNav,{
+    root: null,
+    threshold: 0,
+    rootMargin: `-90px`
+})
+headerObserver.observe(header)
